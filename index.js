b756e9492fa7a31192e6d68b385d859a5da139b6
MamadJS <hassanhashemi.pro@gmail.com>
2021-11-06 06:27:36 +0330

    change commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-let name = "mamadjs";
-console.log(name)
+function firstfunc() {
+    document.getElementById("demo").style.color = "blue";
+    document.getElementById("demo").style.fontSize = "55px";
+    document.getElementById("demo").style.textAlign = "center";
+}
+function reset() {
+     document.getElementById("demo").style.color = "black";
+    document.getElementById("demo").style.fontSize = "16px";
+}
+let count = 42;
+count + 2;
+console.log(count);
